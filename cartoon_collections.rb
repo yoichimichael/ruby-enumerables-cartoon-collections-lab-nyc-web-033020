ee3c52f628bc697973a8a269798b2c9c55b45ac9
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-28 08:59:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(words)
   new_call = words.map { |word| word.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_words)
+  call_words.find { |word| word.length > 4 }
 end
 
 def find_the_cheese# code an argument here
