fca23eeda5a1b9d9f04e356cd599c4ed2135cae4
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-28 08:50:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(group)
+  group.each_with_index { |ele, idx| puts "#{idx} #{ele}" } 
 end
 
 def summon_captain_planet# code an argument here
