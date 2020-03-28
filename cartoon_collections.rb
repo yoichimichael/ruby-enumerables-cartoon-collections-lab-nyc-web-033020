7dff48054d68f834fe5fef81bd656e1568beb301
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-28 09:27:43 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(group)
+  group.each_with_index { |ele, idx| puts "#{idx + 1} #{ele}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(words)
+  new_call = words.map { |word| word.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_words)
+    call_words.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find { |item| cheese_types.include?(item) }
 end
